f8e61ea181b6c0a2509771fe9e7cb53e9fd21846
John Arvin Aboque <arvinaboque@voyagers-MacBook-Pro.local>
2016-06-27 13:52:06 +0800

    First Commit

---
+++ src/main/java/todo/Application.java
@@ -0,0 +1,15 @@
+package todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by arvinaboque on 6/27/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
