55af8c5767ffac58810fb50f5be7f0bbb9a0d84c
John Arvin Aboque <arvinaboque@voyagers-MacBook-Pro.local>
2016-06-28 11:12:07 +0800

    Add addTask

--- src/main/java/todo/controller/TaskController.java
+++ src/main/java/todo/controller/TaskController.java
@@ -1,11 +1,43 @@
 package todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import todo.service.RepositoryService;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by arvinaboque on 6/27/16.
  */
 @RestController
 public class TaskController {
+
+    private RepositoryService repositoryService;
+
+    @Autowired
+    TaskController(RepositoryService repositoryService){
+        this.repositoryService = repositoryService;
+    }
+
+    @RequestMapping(value="/task/add", method= RequestMethod.POST)
+    public Map<String, Object> addTask(@RequestBody Map<String,Object> taskMap){
+
+        String title = taskMap.get("title").toString();
+        String description = taskMap.get("description").toString();
+        Date createdDate = Date.from(Instant.now());
+
+        repositoryService.addTask( 1l, title, description, createdDate.toString());
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("message", "Task created successfully");
+
+        return response;
+    }
 
 }
