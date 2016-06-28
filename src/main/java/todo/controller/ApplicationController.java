b90641fae9127776f129287b3e8001f7ebd251fe
John Arvin Aboque <arvinaboque@voyagers-MacBook-Pro.local>
2016-06-28 10:40:36 +0800

    Add addUser

--- src/main/java/todo/controller/ApplicationController.java
+++ src/main/java/todo/controller/ApplicationController.java
@@ -1,12 +1,12 @@
 package todo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by arvinaboque on 6/27/16.
  */
-@Controller
+@RestController
 public class ApplicationController {
 
     @RequestMapping(value="/")
--- src/main/java/todo/controller/UserController.java
+++ src/main/java/todo/controller/UserController.java
@@ -1,20 +1,29 @@
 package todo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import todo.model.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by arvinaboque on 6/27/16.
  */
-@Controller
+@RestController
 public class UserController {
 
 
-    @RequestMapping(value="/users", method= RequestMethod.POST)
+    @RequestMapping(value="/user/add", method = RequestMethod.POST)
+    public Map<String, Object> addUser(@RequestBody Map<String, Object> userMap){
+
+
+        return null;
+    }
+
+    @RequestMapping(value="/users", method = RequestMethod.GET)
     public List<User> getUsers(){
         return null;
     }
