0be4f4c4fc4270ad0314ddcd54fa1c0ed673445b
Bill Villaflor <bill.villaflor@ymail.com>
2016-06-29 12:00:49 +0800

    pinagandang URL mapping

--- src/main/java/todo/controller/TaskController.java
+++ src/main/java/todo/controller/TaskController.java
@@ -1,10 +1,7 @@
 package todo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import todo.model.Task;
 import todo.service.RepositoryService;
 
@@ -17,6 +14,7 @@ import static todo.controller.ControllerHelper.*;
  * Created by arvinaboque on 6/27/16.
  */
 @RestController
+@RequestMapping("/user/{userId}/task")
 public class TaskController {
 
     private RepositoryService repositoryService;
@@ -27,63 +25,55 @@ public class TaskController {
         this.repositoryService = repositoryService;
     }
 
-    @RequestMapping( value="/task/add", method= RequestMethod.POST )
-    public Map<String, Object> addTask( @RequestBody Map<String,Object> taskMap ){
+    @RequestMapping( value="/add", method= RequestMethod.POST )
+    public Map<String, Object> addTask( @PathVariable Long userId, @RequestBody Map<String,Object> taskMap ){
 
         String title = taskMap.get( TASK_TITLE ).toString();
         String description = taskMap.get( TASK_DESCRIPTION ).toString();
-        Long userId = Long.valueOf( taskMap.get( USER_ID ).toString() );
 
         repositoryService.addTask( userId, title, description, getDateNow() );
 
         return createResponse( "Task created successfully" );
     }
 
-    @RequestMapping( value="/task/delete", method = RequestMethod.POST )
-    public Map<String, Object> deleteTask( @RequestBody Map<String, Object> taskMap ){
+    @RequestMapping( method = RequestMethod.GET )
+    public Map<String, Object> getTasks( @PathVariable Long userId ){
 
-        Long taskId = Long.valueOf( taskMap.get( TASK_ID ).toString() );
+        List<Task> tasks = repositoryService.getTasks( Long.valueOf( userId ) );
+
+        Map<String,Object> response = createResponse( "Fetched tasks" );
+        response.put( "task", tasks );
+
+        return response;
+    }
+
+    @RequestMapping( value="/{taskId}/delete", method = RequestMethod.DELETE )
+    public Map<String, Object> deleteTask( @PathVariable Long taskId ){
 
         repositoryService.deleteTasks( taskId );
 
         return createResponse( "Task deleted successfully" );
     }
 
-    @RequestMapping( value="/task/complete", method = RequestMethod.POST )
-    public Map<String, Object> completeTask( @RequestBody Map<String, Object> taskMap ){
+    @RequestMapping( value="/{taskId}/complete", method = RequestMethod.PUT )
+    public Map<String, Object> completeTask( @PathVariable Long taskId ){
 
-        Long taskId = Long.valueOf( taskMap.get( TASK_ID ).toString() );
         repositoryService.completeTask( taskId );
 
         return createResponse( "Task completed successfully" );
     }
 
-    @RequestMapping( value="/task/reopen", method = RequestMethod.POST )
-    public Map<String, Object> reopenTask( @RequestBody Map<String, Object> taskMap ){
+    @RequestMapping( value="/{taskId}/reopen", method = RequestMethod.PUT )
+    public Map<String, Object> reopenTask( @PathVariable Long taskId ){
 
-        Long taskId = Long.valueOf( taskMap.get( TASK_ID ).toString() );
         repositoryService.reopenTask( taskId );
 
         return createResponse( "Task reopened successfully" );
     }
 
-    @RequestMapping( value="/task", method = RequestMethod.POST )
-    public Map<String, Object> getTasks( @RequestBody Map<String,Object> taskMap ){
+    @RequestMapping( value="/{taskId}/edit", method = RequestMethod.PUT )
+    public Map<String,Object> editTask( @PathVariable Long taskId, @RequestBody Map<String, Object> taskMap ){
 
-        Long userId = Long.valueOf( taskMap.get( USER_ID ).toString() );
-        List<Task> tasks = repositoryService.getTasks(Long.valueOf( userId ));
-
-
-        Map<String,Object> response = createResponse( "Fetched tasks" );
-        response.put("task", tasks);
-
-        return response;
-    }
-
-    @RequestMapping( value="/task/edit", method = RequestMethod.POST )
-    public Map<String,Object> editTask( @RequestBody Map<String, Object> taskMap ){
-
-        Long taskId = Long.valueOf(taskMap.get( TASK_ID ).toString());
         String title = taskMap.get( TASK_TITLE ).toString();
         String description = taskMap.get( TASK_DESCRIPTION ).toString();
 
