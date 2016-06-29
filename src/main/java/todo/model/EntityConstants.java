f45823a9b32e1fef05541af50624690e00fca24e
John Arvin Aboque <arvinaboque@voyagers-MacBook-Pro.local>
2016-06-29 18:56:12 +0800

    Working add task

--- src/main/java/todo/model/EntityConstants.java
+++
@@ -1,9 +0,0 @@
-package todo.model;
-
-/**
- * Created by bill.villaflor on 6/28/16.
- */
-public final class EntityConstants {
-
-
-}
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,7 +1,15 @@
 angular.module('todor', [])
   .controller('data', function($scope, $http) {
+
       $http.get('/user/1/task').success(function(data) {
         console.data;
         $scope.task = data;
       })
+
+      $scope.addTask = function(){
+        $http.post('/user/1/task/add', $scope.form).success(function(data){
+            console.log("add success");
+        });
+      };
+
 });
